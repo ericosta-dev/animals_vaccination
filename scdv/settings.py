7d06f82a2a553770b5b7a9cfcf1b6f0abb29a261
Eric Costa <ericosta.dev@gmail.com>
2022-07-10 03:04:38 -0300

    Ajuste de gitignore e configuração de email mais segura

--- scdv/settings.py
+++ scdv/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -158,4 +159,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'erictargino@gmail.com'
-EMAIL_HOST_PASSWORD = 'dmithxyirmfhsotq'
+EMAIL_HOST_PASSWORD = config('MAIL_PASSWORD')
